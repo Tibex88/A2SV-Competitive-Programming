8e396cc585a82f1c5a7e0f70b04528ddfa6fdc7c
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2022-10-04 13:53:37 +0300

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let answer = []
+    for(let i=1; i<=n ;i++){
+        if(i%3 === 0 & i%5===0 )  answer.push(`FizzBuzz`);
+        else if(i%3 === 0 )  answer.push(`Fizz`);
+        else if( i%5===0 )  answer.push(`Buzz`);
+        else{
+            answer.push(`${i}`)
+        }
+    }
+    return answer
+};
