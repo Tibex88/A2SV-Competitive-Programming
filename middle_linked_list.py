1b95e9cb430b70b34c19dbe7bb11e76efd85a525
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2023-01-24 15:37:22 +0300

    add

---
+++ middle_linked_list.py
@@ -0,0 +1,15 @@
+#  https://leetcode.com/problems/middle-of-the-linked-list/submissions/884344287/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = p2 = head
+
+        while p2 and p2.next:
+            p2 = p2.next.next
+            p1 = p1.next        
+
+        return p1
